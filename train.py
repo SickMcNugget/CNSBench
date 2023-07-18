83aacab0a2a4bd4ff04ed61a70cbeb89c86051e3
Chaki Ramesh <chakramesh1@gmail.com>
2023-07-18 17:04:38 +0800

    Added mmseg functionality

--- train.py
+++ train.py
@@ -1,10 +1,201 @@
 import argparse
 from pathlib import Path
-import time
+import datetime
 import importlib
+import mmcv
+import mmseg
+from mmseg.registry import DATASETS
+from mmseg.datasets import BaseSegDataset
+from mmengine import Config
+from mmengine.runner import Runner
+
+def get_config(dataset: str, normalised: bool, model: str):
+    config = ""
+    match(dataset.lower()):
+        case 'monuseg':
+            match(model.lower()):
+                case 'deeplabv3plus':
+                    if(normalised):
+                        config = "1"
+                    else:
+                        config = "deeplabv3plus_r50-d8_1xb1-20k_monuseg-512x512.py"
+                case 'unet':
+                    if(normalised):
+                        config = "3"
+                    else:
+                        config = "4"
+                case default:
+                    return "Model chosen does not exist"
+        case 'monusac':
+            match(model.lower()):
+                case 'deeplabv3plus':
+                    if(normalised):
+                        config = "5"
+                    else:
+                        config = "deeplabv3plus_r50-d8_1xb1-20k_monusac-512x512.py"
+                case 'unet':
+                    if(normalised):
+                        config = "7"
+                    else:
+                        config = "8"
+                case default:
+                    return "Model chosen does not exist"
+        case 'cryonuseg':
+            match(model.lower()):
+                case 'deeplabv3plus':
+                    if(normalised):
+                        config = "9"
+                    else:
+                        config = "deeplabv3plus_r50-d8_1xb1-20k_cryonuseg-512x512.py"
+                case 'unet':
+                    if(normalised):
+                        config = "11"
+                    else:
+                        config = "12"
+                case default:
+                    return "Model chosen does not exist"
+        case 'tnbc':
+            match(model.lower()):
+                case 'deeplabv3plus':
+                    if(normalised):
+                        config = "13"
+                    else:
+                        config = "deeplabv3plus_r50-d8_1xb1-20k_tnbc-512x512.py"
+                case 'unet':
+                    if(normalised):
+                        config = "15"
+                    else:
+                        config = "16"
+                case default:
+                    return "Model chosen does not exist"
+        case default:
+            return "Dataset chosen does not exist"
+    return config
+
+def register_datasets():
+    @DATASETS.register_module()
+    class MonusegDataset(BaseSegDataset):
+        """MoNuSeg dataset."""
+        METAINFO = dict(
+            classes = ('background', 'nucleus'),
+            #palette = [[120,120,120], [180, 120, 120]]
+            palette = [[120,120,120], [255, 255, 255]] # This has been changed, lets see what it does
+        )
+    
+        def __init__(self,
+                 img_suffix='.png',
+                 seg_map_suffix='.png',
+                 reduce_zero_label=False,
+                 **kwargs) -> None:
+            super().__init__(
+                img_suffix=img_suffix, 
+                seg_map_suffix=seg_map_suffix,
+                reduce_zero_label=reduce_zero_label,
+                **kwargs)
+    
+    @DATASETS.register_module()
+    class MonusacDataset(BaseSegDataset):
+        """MoNuSAC dataset."""
+        METAINFO = dict(
+            classes = ('background', 'nucleus'),
+            #palette = [[120,120,120], [180, 120, 120]]
+            palette = [[120,120,120], [255, 255, 255]] # This has been changed, lets see what it does
+        )
+    
+        def __init__(self,
+                 img_suffix='.png',
+                 seg_map_suffix='.png',
+                 reduce_zero_label=False,
+                 **kwargs) -> None:
+            super().__init__(
+                img_suffix=img_suffix, 
+                eg_map_suffix=seg_map_suffix,
+                reduce_zero_label=reduce_zero_label,
+                **kwargs)
+        
+    @DATASETS.register_module()
+    class CryonusegDataset(BaseSegDataset):
+        """CryoNuSeg dataset."""
+        METAINFO = dict(
+            classes = ('background', 'nucleus'),
+            #palette = [[120,120,120], [180, 120, 120]]
+            palette = [[120,120,120], [255, 255, 255]] # This has been changed, lets see what it does
+        )
+    
+        def __init__(self,
+                 img_suffix='.png',
+                 seg_map_suffix='.png',
+                 reduce_zero_label=False,
+                 **kwargs) -> None:
+            super().__init__(
+                img_suffix=img_suffix, 
+                seg_map_suffix=seg_map_suffix,
+                reduce_zero_label=reduce_zero_label,
+                **kwargs)
+    
+    @DATASETS.register_module()
+    class TnbcDataset(BaseSegDataset):
+        """TNBC dataset."""
+        METAINFO = dict(
+            classes = ('background', 'nucleus'),
+            #palette = [[120,120,120], [180, 120, 120]]
+            palette = [[120,120,120], [255, 255, 255]] # This has been changed, lets see what it does
+        )
+    
+        def __init__(self,
+                 img_suffix='.png',
+                 seg_map_suffix='.png',
+                 reduce_zero_label=False,
+                 **kwargs) -> None:
+            super().__init__(
+                img_suffix=img_suffix, 
+                seg_map_suffix=seg_map_suffix,
+                reduce_zero_label=reduce_zero_label,
+                **kwargs)
 
 def main(args: argparse.Namespace):
+    #print(args.integers)
+    register_datasets() # make sure the datasets are registered
+    # -- grab the config location based on arguments -- #
+    config_location = get_config(args.dataset, args.normalised, args.model)
+    cfg = Config.fromfile(config_location)
+    print('[*] Config loaded: ' + config_location)
     
+    # -- set batch size for model -- #
+    cfg.train_dataloader.batch_size = args.batch
+
+    # -- Create the work directory -- #
+    now = datetime.datetime.now()
+    work_dir = "work_dirs/" + args.dataset + "/" + args.model + "/" + now.year + "/" + now.month + "/" + now.day + "/" + now.hour + "/" + now.minute + "/"
+    cfg.work_dir = work_dir
+
+    # -- Setup other model config info -- #
+    cfg.train_cfg.max_iters = args.iteration
+    cfg.train_cfg.val_interval = args.val
+    cfg.default_hooks.logger.interval = args.log_interval
+    cfg.default_hooks.checkpoint.interval = args.checkpoint
+
+    # -- WANDB STUFF [need to change] -- #
+    if (args.wandb):
+        project="CryoNuSeg"                                                     #change this
+        name="deeplabv3+_iter_20000"  
+        cfg.vis_backends = [dict(
+            type='WandbVisBackend',
+            init_kwargs = dict(project=project, name=name))]                #change this
+    
+    # -- Visualiser -- #
+    cfg.visualizer = dict(
+        type='SegLocalVisualizer', vis_backends=cfg.vis_backends, name='visualizer')
+    
+    # -- Set seed to facilitate reproducing the result -- #
+    cfg['randomness'] = dict(seed=0)
+
+    # -- Let's have a look at the final config used for training -- #
+    print(f'Config:\n{cfg.pretty_text}')
+
+    # -- Train a model -- #
+    runner = Runner.from_cfg(cfg)
+    runner.train()
 
 def get_mmseg_root():
     mmseg = importlib.util.find_spec("mmseg")
@@ -15,17 +206,23 @@ def get_mmseg_root():
     return root
 
 def get_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--config", type=Path, help="The configuration file to use for training")
-    parser.add_argument("--project", type=str, help="The name to use for saving project data")
-    parser.add_argument("--batch-size", type=int, default=2, help="The batch size to use during training")
+    parser = argparse.ArgumentParser("Training script for training models on the mmsegmentation architecture")
+    parser.add_argument("-c", "--config", type=Path, required=True, help="The configuration file to use for training")
+    #parser.add_argument("--project", type=str, required=True, help="The name to use for saving project data")
+    parser.add_argument("--batch", type=int, default=2, help="The batch size to use during training")
     parser.add_argument("--wandb", action="store_true", help="Enables Weights and Biases for logging results")
-
+    parser.add_argument("--dataset", type=str, required=True, help="The name of the dataset to use for training [ 'MoNuSeg' , 'CryoNuSeg', 'MoNuSAC', 'TNBC']")
+    parser.add_argument("--model", type=str, required=True, help="The name of the model to use for training ['deeplabv3plus','unet']")
+    parser.add_argument("--normalised", action='store_true', help="Flag for training on normalised version of dataset")
+    parser.add_argument("--iteration ", type=int, default=20000, help="Max iterations the model should train upto")
+    parser.add_argument("--val", type=int, default=1000, help="validation interval for model training")
+    parser.add_argument("--log_interval", type=int, default=10, help="Iteration inverval for tracking current model progress")
+    parser.add_argument("--checkpoint", type=int, default=5000, help="Model will save every <checkpoint> iterations")
     args = parser.parse_args()
     return args
 
 if __name__=="__main__":
     args = get_args()
-
     # Ensure old profiling data is cleaned up
     main(args)
+
