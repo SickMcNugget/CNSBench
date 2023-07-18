a781b3058341a1586d35c2094b5a45888195017d
SickMcNugget <jnregan00@hotmail.com>
2023-07-19 01:53:32 +0800

    Change config paradigm

---
+++ configs/normalised_dataset.py
@@ -0,0 +1,16 @@
+__base__ = ["default_dataset.py"]
+
+img_dir = "yolo_sn"
+
+train_dataloader = dict(
+    dataset=dict(
+        data_prefix=dict(
+            img_path=f'{img_dir}/train')))
+val_dataloader = dict(
+    dataset=dict(
+        data_prefix=dict(
+            img_path=f'{img_dir}/val')))
+test_dataloader = dict(
+    dataset=dict(
+        data_prefix=dict(
+            img_path=f'{img_dir}/test')))
---
+++ configs/unet.py
@@ -0,0 +1,1 @@
+_base_ = ['mmseg::_base_/models/fcn_unet_s5-d16.py']
