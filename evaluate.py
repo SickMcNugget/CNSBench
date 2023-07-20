2eb4278c08051786239d49ab75e0cd27c9ee478c
SickMcNugget <jnregan00@hotmail.com>
2023-07-20 13:31:55 +0800

    Simplified evaluation

--- evaluate.py
+++ evaluate.py
@@ -1,42 +1,73 @@
 import argparse
 from pathlib import Path
-# import cnsbench.metrics
-from cnsbench.evaluation import Comparer
+
+import pandas as pd
+from cnsbench import metrics
+import cv2
 
 def main(args: argparse.Namespace):
     # args.compare_root = "final_preds/nostainnorm/MoNuSAC/unet/"
     # args.dataset_root = "MoNuSAC/"
-    evaluate_dataset(args)
+    maskdir = get_maskdir(args)
+    gt_paths = sorted(maskdir.glob("*.png"))
+    pred_paths = sorted(args.compare_root.glob("*.png"))
 
-def evaluate_dataset(args: argparse.Namespace):
-    comparer = Comparer(args.dataset_root)
-    # df = comparer.compare("../NucleiSegmentation_mmseg/export")
-    df = comparer.compare(args.compare_root)
-    print(df)
-    print(df.groupby("split").mean().loc[:, "f1":"iou"].round(3))
+    results = {}
+    for gt_path, pred_path in zip(gt_paths, pred_paths):
+        if gt_path.name != pred_path.name:
+            print("Woah there buddy something seems to have gone awry")
 
-        # zip_paths = downloader_cls().download()
-    
-        # print(f"\nUnzipping {dataset}")
-        # unzip_paths = Unzipper(zip_paths).unzip()
 
-        # print(f"\nOrganising {dataset}")
-        # mover_cls = get_dataset_class(dataset, "Mover")
-        # mover_cls(args.dataset_root, unzip_paths).move_all()
+        gt = cv2.imread(str(gt_path), cv2.IMREAD_GRAYSCALE)
+        pred = cv2.imread(str(pred_path), cv2.IMREAD_GRAYSCALE)
 
-        # print(f"\nGenerate masks for {dataset}")
-        # mask_generator_cls = get_dataset_class(dataset, "MaskGenerator")
-        # mask_generator_cls(args.dataset_root).generate_masks()
+        comparisons = {}
+        comparisons["name"] = gt_path.stem
 
-        # print(f"\nCreating YOLO compatible training data for {dataset}\n")
-        # yolofier_cls = get_dataset_class(dataset, "Yolofier")
-        # yolofier_cls(args.dataset_root).yolofy()
+        parts = gt_path.parts
+        if "train" in parts:
+            comparisons["split"] = "train"
+        elif "val" in parts:
+            comparisons["split"] = "val"
+        elif "test" in parts:
+            comparisons["split"] = "test"
 
+        comparisons["accuracy"] = metrics.calc_accuracy(gt, pred)
+        comparisons["precision"] = metrics.calc_precision(gt, pred)
+        comparisons["recall"] = metrics.calc_recall(gt, pred)
+        comparisons["f1"] = metrics.calc_f1(comparisons["precision"], comparisons["recall"])
+        comparisons["iou"] = metrics.calc_iou(gt, pred)
+        # comparisons["hausdorff"] = metrics.calc_hausdorff(gt, pred)
+
+        for key, value in comparisons.items():
+            if not key in results:
+                results[key] = []
+            results[key].append(value)
+
+        df = pd.DataFrame.from_dict(results)
+        df.set_index("name", inplace=True)
+        
+        print(df)
+        print(df.groupby("split").mean().loc[:, "f1":"iou"].round(3))
+
+#def evaluate_dataset(args: argparse.Namespace):
+#    comparer = Comparer(args.dataset_root)
+#    # df = comparer.compare("../NucleiSegmentation_mmseg/export")
+#    df = comparer.compare(args.compare_root)
+#    print(df)
+#    print(df.groupby("split").mean().loc[:, "f1":"iou"].round(3))
+
+def get_maskdir(args: argparse.Namespace):
+    maskdir = args.dataset_root / args.dataset / "masks" / "test"
+    return Path(maskdir)
 
 def get_args() -> argparse.Namespace:
+    DATASETS = ["MoNuSeg", "MoNuSAC", "CryoNuSeg", "TNBC"]
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("--compare-root", default=Path("."), type=Path, help="The path where predictions lie")
-    parser.add_argument("--dataset-root", default=Path("."), type=Path, help="The path to output the dataset")
+    parser.add_argument("--dataset", type=str, choices=DATASETS, required=True, help="The dataset to use when comparing masks")
+    parser.add_argument("--compare-root", type=Path, required=True, help="The path where predictions lie")
+    parser.add_argument("--dataset-root", type=Path, default=Path("datasets"), help="The path where datasets are stored")
     args = parser.parse_args()
     
     return args
