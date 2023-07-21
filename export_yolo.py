bac328fce9a70924104ca1e1c738facc19e9f819
SickMcNugget <jnregan00@hotmail.com>
2023-07-21 18:55:12 +0800

    Check for no results

--- export_yolo.py
+++ export_yolo.py
@@ -38,13 +38,17 @@ def main(args: argparse.Namespace):
 
     for result in results:
         name = Path(result.path).name
-        masks = result.masks.data
-        nuclei_mask = torch.any(masks, dim=0).to(torch.uint8)
-        nuclei_mask = nuclei_mask.reshape(1, 1, *nuclei_mask.shape)
         height, width = result.orig_img.shape[:2]
+        
+        if result.masks is not None:
+            masks = result.masks.data
+            nuclei_mask = torch.any(masks, dim=0).to(torch.uint8)
+            nuclei_mask = nuclei_mask.reshape(1, 1, *nuclei_mask.shape)
 
-        # Tested all interpolation modes, Nearest neighbour best represents prediction (at least for square images)
-        nuclei_mask = T.Resize(size=(height, width), interpolation=T.InterpolationMode.NEAREST, antialias=False)(nuclei_mask).squeeze()
+            # Tested all interpolation modes, Nearest neighbour best represents prediction (at least for square images)
+            nuclei_mask = T.Resize(size=(height, width), interpolation=T.InterpolationMode.NEAREST, antialias=False)(nuclei_mask).squeeze()
+        else:
+            nuclei_mask = torch.zeros((height, width))
         
         image_path = str(save_path / name)
         cv2.imwrite(image_path, nuclei_mask.cpu().numpy())
@@ -52,14 +56,16 @@ def main(args: argparse.Namespace):
 def get_args() -> argparse.Namespace:
     DATASETS = ["MoNuSeg", "MoNuSAC", "TNBC", "CryoNuSeg"]
     parser = argparse.ArgumentParser("Training script for the Ultralytics YOLOv8 model")
-    parser.add_argument("--dataset", type=str, required=True, choices=DATASETS, help="The dataset to use for training")
-    parser.add_argument("--model", type=Path, required=True, help="The path to the trained model to use in inference")
+    parser.add_argument("--dataset", type=str, required=False, choices=DATASETS, help="The dataset to use for training")
+    parser.add_argument("--model", type=Path, required=False, help="The path to the trained model to use in inference")
     parser.add_argument("--max-det", type=int, default=1600, help="The maximum number of instances that can be predicted")
     parser.add_argument("--dataset-root", type=Path, default=Path("datasets"), help="The path to where datasets are stored")
     parser.add_argument("--normalised", action='store_true', help="Whether to train on stain normalised images")
     parser.add_argument("--out-dir", type=Path, default=Path("yolov8_work_dirs"), help="The base directory for saving yolov8 training results")
 
     args = parser.parse_args()
+    args.model = Path("yolov8_work_dirs/MoNuSAC/yolov8l_1xb2-500e_monusac-640x640/weights/best.pt")
+    args.dataset = "MoNuSAC"
     return args
 
 if __name__=="__main__":
